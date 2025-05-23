4de2c0c3a629cde34f88867335f14d283a9e69ef
Jhon Jairo Arango Hurtado <39918619+jjah37@users.noreply.github.com>
2025-05-23 10:09:59 -0500

    Add files via upload
    El volumen de una esfera se calcula con la siguiente fórmula matemática:
    
    𝑉 = 4 /  3𝜋𝑟3
    
    Donde:
    
    𝑉
     es el volumen de la esfera,
    
    𝑟
     es el radio de la esfera,
    
    𝜋
     es aproximadamente 3.1416.

---
+++ esfera.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*- 
+#Decoración: Nombre del Algoritmo 
+print("-------------------------------------------------------") 
+print("Complemento1: CALCULAR VOLUMEN DE LA ESFERA.") 
+print("-------------------------------------------------------") 
+ 
+#Constantes 
+PI = 3.1416 
+ 
+#Entradas 
+r = float( input("Ingrese Radio: ")) 
+ 
+#Proceso 
+v = 4/3 * PI * r**3 
+ 
+#Salida 
+print("\nSALIDA: ") 
+print("-------------------------------------------------------") 
+print("El volumen de la esfera es:", v) 
